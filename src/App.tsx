0c22e75c64001026af3749d91a87595d1ff77cbb
Irfanul Hasan <irfan.dev.bd@gmail.com>
2023-07-10 19:04:51 +0600

    react flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import ReactFlow from './react-flow/ReactFlow';
 import GraphView from './react_d3_tree/GraphView';
 
 function App() {
   return (
     <div >
       <header>
-        <GraphView/>
+        <ReactFlow/>
       </header>
     </div>
   );
